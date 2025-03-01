612ddae412afcf38b5b33908183377fd28e446ad
Telmo Barros <somebodyceo@gmail.com>
2025-03-01 21:08:01 +0100

    implement functionality to add a new task

---
+++ app/src/main/java/com/example/myapplication/DialogCloseListener.java
@@ -0,0 +1,8 @@
+package com.example.myapplication;
+
+import android.content.DialogInterface;
+
+public interface DialogCloseListener
+{
+    public void handleDialogClose(DialogInterface dialog);
+}
