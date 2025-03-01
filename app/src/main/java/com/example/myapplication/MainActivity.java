4db4c004111e88daf9454dc2d02bad572c73b501
Telmo Barros <somebodyceo@gmail.com>
2025-03-01 15:58:11 +0100

    implement app startup with dummy data

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.myapplication.Adapter.ToDoAdapter;
+import com.example.myapplication.Model.ToDoModel;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView tasksRecyclerView;
+    private ToDoAdapter taskAdapter;
+    private List<ToDoModel> taskList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        tasksRecyclerView = findViewById(R.id.taskRecyclerView);
+        tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        taskAdapter = new ToDoAdapter(this);
+        tasksRecyclerView.setAdapter(taskAdapter);
+        taskList = new ArrayList<>();
+
+        // Dummy data
+        ToDoModel task = new ToDoModel();
+        task.setTask("Sample task");
+        task.setStatus(0);
+        task.setId(1);
+
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskAdapter.setTasks(taskList);
     }
 }
